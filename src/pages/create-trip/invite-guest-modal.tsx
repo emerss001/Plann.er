3b1d07e79518b748fd0cff039f9b3d87703e3153
emerss001 <emersonnevess80@gmail.com>
2024-07-24 23:22:42 -0300

    adicionando modal de criar link e convidados

--- src/pages/create-trip/invite-guest-modal.tsx
+++ src/pages/create-trip/invite-guest-modal.tsx
@@ -1,6 +1,12 @@
 import { AtSign, Plus, X } from "lucide-react";
 import { FormEvent } from "react";
 import { Button } from "../../components/button";
+import { ModalRoot } from "../../components/Modal/ModalRoot";
+import { ModalTitle } from "../../components/Modal/ModalTitle";
+import { ModalClose } from "../../components/Modal/ModalClose";
+import { ModalDescription } from "../../components/Modal/ModalDescription";
+import { ModalForm } from "../../components/Modal/ModalForm";
+import { ModalInput } from "../../components/Modal/ModalInput";
 
 interface InviteGuestsModalProps {
   closeGuestModal: () => void;
@@ -16,20 +22,20 @@ export function InviteGuestsModal({
   removeEmailFromInvites,
 }: InviteGuestsModalProps) {
   return (
-    <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
-      <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
-        <div className="space-y-2">
-          <div className="flex items-center justify-between">
-            <h2 className="text-lg font-semibold">Selecionar convidados</h2>
-            <button type="button" onClick={closeGuestModal}>
-              <X className="size-5 text-zinc-400" />
-            </button>
-          </div>
-          <p className="text-sm text-zinc-400">
-            Os convidados irão receber e-mails para confirmar a participação na
-            viagem.
-          </p>
+    <ModalRoot>
+      <div className="space-y-2">
+        <div className="flex items-center justify-between">
+          <ModalTitle title="Selecionar convidados" />
+          <ModalClose closeModal={closeGuestModal} />
         </div>
+      </div>
+
+      <ModalDescription>
+        <p className="text-sm text-zinc-400">
+          Os convidados irão receber e-mails para confirmar a participação na
+          viagem.
+        </p>
+
         <div className="flex flex-wrap gap-2">
           {emailsToInvite.map((email) => {
             return (
@@ -48,27 +54,25 @@ export function InviteGuestsModal({
             );
           })}
         </div>
+      </ModalDescription>
 
-        <div className="w-full h-px bg-zinc-800" />
+      <div className="w-full h-px bg-zinc-800" />
 
-        <form
-          onSubmit={addNewEmailToInvite}
-          className="p-2.5 bg-zinc-950 border-zinc-800 rounded-lg flex items-center gap-2"
-        >
-          <AtSign className="size-5 text-zinc-400" />
-          <input
+      <ModalForm onSubmit={addNewEmailToInvite}>
+        <div className="bg-zinc-950 border-zinc-800 rounded-lg flex items-center gap-2 justify-between pr-4">
+          <ModalInput
+            icon={AtSign}
             type="email"
             name="email"
             placeholder="Digite o e-mail do convidado"
-            className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
           />
 
           <Button type="submit" variant="primary">
             Convidar
             <Plus className="size-5" />
           </Button>
-        </form>
-      </div>
-    </div>
+        </div>
+      </ModalForm>
+    </ModalRoot>
   );
 }
--- src/pages/trip-details/create-link-modal.tsx
+++ src/pages/trip-details/create-link-modal.tsx
@@ -1,8 +1,14 @@
-import { Link2, Tag, X } from "lucide-react";
+import { Link2, Tag } from "lucide-react";
 import { Button } from "../../components/button";
 import { FormEvent } from "react";
 import { useParams } from "react-router-dom";
 import { api } from "../../lib/axios";
+import { ModalRoot } from "../../components/Modal/ModalRoot";
+import { ModalTitle } from "../../components/Modal/ModalTitle";
+import { ModalClose } from "../../components/Modal/ModalClose";
+import { ModalDescription } from "../../components/Modal/ModalDescription";
+import { ModalForm } from "../../components/Modal/ModalForm";
+import { ModalInput } from "../../components/Modal/ModalInput";
 
 interface CreateLinkModalProps {
   closeCreateLinkModal: () => void;
@@ -30,45 +36,38 @@ export function CreateLinkModal({
   }
 
   return (
-    <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
-      <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
-        <div className="space-y-2">
-          <div className="flex items-center justify-between">
-            <h2 className="text-lg font-semibold">Cadastrar link</h2>
-            <button type="button" onClick={closeCreateLinkModal}>
-              <X className="size-5 text-zinc-400" />
-            </button>
-          </div>
-          <p className="text-sm text-zinc-400">
-            Todos os convidados podem visualizar os links.
-          </p>
+    <ModalRoot>
+      <div className="space-y-2">
+        <div className="flex items-center justify-between">
+          <ModalTitle title="Cadastrar link" />
+          <ModalClose closeModal={closeCreateLinkModal} />
         </div>
-
-        <form onSubmit={createLink} className="space-y-3">
-          <div className="h-14 px-4 bg-zinc-950 border-zinc-800 rounded-lg flex items-center gap-2">
-            <Tag className="size-5 text-zinc-400" />
-            <input
-              name="title"
-              placeholder="Qual titulo do link?"
-              className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
-            />
-          </div>
-
-          <div className="h-14 flex-1 px-4 bg-zinc-950 border-zinc-800 rounded-lg flex items-center gap-2">
-            <Link2 className="size-5 text-zinc-400" />
-            <input
-              type="url"
-              name="url"
-              placeholder="Insira aqui o link"
-              className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
-            />
-          </div>
-
-          <Button variant="primary" size="full">
-            Salvar link
-          </Button>
-        </form>
       </div>
-    </div>
+
+      <ModalDescription>
+        <p className="text-sm text-zinc-400">
+          Todos os convidados podem visualizar os links.
+        </p>
+      </ModalDescription>
+
+      <ModalForm onSubmit={createLink}>
+        <ModalInput
+          icon={Tag}
+          name="title"
+          placeholder="Qual titulo do link?"
+        />
+
+        <ModalInput
+          icon={Link2}
+          type="url"
+          name="url"
+          placeholder="Insira aqui o link"
+        />
+
+        <Button variant="primary" size="full">
+          Salvar link
+        </Button>
+      </ModalForm>
+    </ModalRoot>
   );
 }
